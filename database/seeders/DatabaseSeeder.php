ec0c4e650696357072574beceadc463d5bf1b921
Tanver Mahidi <tanveermahidi@gmail.com>
2026-05-13 18:41:32 +0600

    one to many learnd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\info;
+use App\Models\Skill;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,8 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+
+    
     public function run()
     {
         // \App\Models\User::factory(10)->create();
@@ -23,25 +26,31 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-for ($i=0; $i <10 ; $i++) { 
-    $user = User::create([
+
+        $data= ['PHP','JS','REACT','Python'];
+
+        foreach($data as $d){
+            Skill::create(["name"=>$d]);
+        }
+        for ($i = 0; $i < 10; $i++) {
+            $user = User::create([
                 'name' => "User $i",
                 'email' => "user$i@example.com",
                 'password' => bcrypt('password123'),
             ]);
-    # code...
-    info::create([
-        "user_id"=>$i,
-        "address"=>"Dhaka $i",
-        "zip_code"=>$i%2==0 ? rand(100,200):null,
-        "phone"=>rand(1000,5000),
-    ]);
-}
+            # code...
+            info::create([
+                "user_id" => $i,
+                "address" => "Dhaka $i",
+                "zip_code" => $i % 2 == 0 ? rand(100, 200) : null,
+                "phone" => rand(1000, 5000),
+            ]);
+        }
 
         $this->call([
             AdminSeeder::class,
             ClientSeeder::class,
-        
+
         ]);
     }
 }
